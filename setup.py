83c282400739394e5098170135e29bcc6caf009d
Muhammad Fawi <m.noor.fawi@gmail.com>
2021-10-15 16:43:25 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("requirements.txt") as rq:
 
 setup(
       name="lshashing",
-      version="1.1.1",
+      version="1.1.2",
       description="Nearest neighbors search using locality-sensitive hashing",
       packages=["lshashing"],
       install_requires=requirements,
